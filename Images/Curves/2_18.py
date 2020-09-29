eb57402a182515243fa2befa495f30b02e9dbbd7
Nekrasov-VV <vsevolod.nekrasov@math.msu.ru>
2020-09-29 22:48:48 +0300

    Using of matplotlib

---
+++ Images/Curves/2_18.py
@@ -0,0 +1,25 @@
+﻿import matplotlib.pyplot as plt
+from numpy import arange
+from numpy import meshgrid
+import seaborn as sns
+plt.rc("text",usetex=True)
+plt.figure(figsize=(5,3))
+#--------------------------------------
+delta = 0.025
+xrange = arange(-5.0, 15.0, delta)
+yrange = arange(-10.0, 10.0, delta)
+X, Y = meshgrid(xrange,yrange)
+
+# F is one side of the equation, G is the other
+F = X**2+Y**2
+G = 8*X
+plt.contour(X, Y, (F - G), [0])
+#--------------------------------------
+F = Y**2
+G = X**3/(2-X)
+plt.contour(X, Y, (F - G), [0], linestyles='dashed',colors="g")
+#--------------------------------------
+plt.axis('off')
+plt.xlim(-8,18)
+plt.ylim(-9,7)
+plt.savefig('2_18.pgf', bbox_inches='tight')
---
+++ Images/Curves/2_19.py
@@ -0,0 +1,14 @@
+﻿from pylab import *
+plt.rc("text",usetex=True)
+plt.figure(figsize=(5,3))
+#--------------------------------------
+x=linspace(-10,10,100)
+y1=(x**2)/4
+y2=8/(x**2+4)
+plot(x,y1, color="r")
+plot(x,y2, color="g")
+#--------------------------------------
+axis('off')
+xlim(-10,10)
+ylim(0,12)
+savefig('2_19.pgf', bbox_inches='tight')
